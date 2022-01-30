75c8416c92240ac4d5dbc24631db0656365dda77
Artem <artem@dobroagency.ru>
2022-01-31 02:01:04 +0300

    Задание 1.7-1.9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -5,30 +5,36 @@
 //!+
 
 // Fetch prints the content found at each specified URL.
+// Упражнения 1.7 - 1.9
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		// Они просили добавить только автоматическое добавление http://, но сейчас 2022 год, все уже перешли на https
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		bytes_count, err := io.Copy(os.Stdout, resp.Body)
+
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%d bytes copied \n", bytes_count)
+		fmt.Printf("Status code: %s", resp.Status)
 	}
 }
-
-//!-
