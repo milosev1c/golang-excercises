758c368da4923baf0ab92a23711481d1b7205720
Artem <artem@dobroagency.ru>
2022-02-25 19:19:55 +0300

    хз какие-то там задачки готовы 39 и 310

---
+++ ch3/comma/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func comma(s string) string {
+	var decPart string
+	c := 0
+	if decPos := strings.LastIndex(s, "."); decPos != -1 {
+		decPart = s[decPos:]
+		s = s[:decPos]
+	}
+	n := len(s)
+	for i := n - 1; i >= 0; i-- {
+		c++
+		if c%3 == 0 && i != 0 {
+			s = s[:i] + "," + s[i:]
+		}
+	}
+	return s + decPart
+}
+
+func main() {
+	fmt.Println(comma("123456"))
+	fmt.Println(comma("123456789"))
+	fmt.Println(comma("123456789.332"))
+	fmt.Println(comma("123"))
+	fmt.Println(comma("3330"))
+}
